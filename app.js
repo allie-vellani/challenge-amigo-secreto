9a1a4f6b4c415e38212b0dc8b31b3e2d7cd344f2
allie-vellani <allie.vellani@outlook.com>
2025-03-06 22:35:47 -0300

    Criação da lógica da aplicação e do README

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nomeAmigo = input.value.trim();
+
+    if (nomeAmigo !== '') {
+        amigos.push(nomeAmigo);
+        console.log(amigos);
+        input.value = '';
+
+    const lista = document.getElementById('listaAmigos');
+    const li = document.createElement('li');
+    li.textContent = nomeAmigo;
+    lista.appendChild(li);
+    } else {
+        alert('Digite um nome válido');
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length <2) {
+        alert('Adicione pelo menos 2 amigos para realizar o sorteio.');
+        return;
+    }
+
+    const sorteio = [];
+    const amigosDisponiveis = [...amigos];
+
+    amigos.forEach((amigo) => {
+        let index;
+        let escolhido;
+
+        do {
+            index = Math.floor(Math.random() * amigosDisponiveis.length);
+            escolhido = amigosDisponiveis[index];
+        } while (escolhido === amigo);
+
+        sorteio.push({ amigo, amigoSecreto: escolhido });
+        amigosDisponiveis.splice(index, 1);
+    });
+
+    exibirResultado(sorteio);
+}
+
+function exibirResultado(sorteio) {
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+
+    sorteio .forEach((par) => {
+        const li = document.createElement('li');
+        li.textContent = `${par.amigo} tirou ${par.amigoSecreto}`;
+        resultado.appendChild(li);
+    });
+}
+    
